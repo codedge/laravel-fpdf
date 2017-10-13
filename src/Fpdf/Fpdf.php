8dac056f8c6f5e78036344daa668e2f1541af3f6
Doug Niccum <dniccumdesign@gmail.com>
2017-10-12 21:21:41 -0500

    Fixes a bug with namespacing

--- src/Fpdf/Fpdf.php
+++ src/Fpdf/Fpdf.php
@@ -268,7 +268,7 @@ function AliasNbPages($alias='{nb}')
 function Error($msg)
 {
 	// Fatal error
-	throw new Exception('FPDF error: '.$msg);
+	throw new \Exception('FPDF error: '.$msg);
 }
 
 function Close()
