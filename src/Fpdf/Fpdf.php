b676e7a2d0588775383ca917c2f0859e2287f1d4
Anwar Sarmiento Ramos <anwarsarmiento@gmail.com>
2018-05-06 11:35:46 -0600

    Size print POS
    para poder tener tamaño de impresoras POS para tiendas y demas

--- src/Fpdf/Fpdf.php
+++ src/Fpdf/Fpdf.php
@@ -127,7 +127,7 @@ function __construct($orientation='P', $unit='mm', $size='A4')
 		$this->Error('Incorrect unit: '.$unit);
 	// Page sizes
 	$this->StdPageSizes = array('a3'=>array(841.89,1190.55), 'a4'=>array(595.28,841.89), 'a5'=>array(420.94,595.28),
-		'letter'=>array(612,792), 'legal'=>array(612,1008));
+		'letter'=>array(612,792), 'legal'=>array(612,1008),'pos'=>array(204,650));
 	$size = $this->_getpagesize($size);
 	$this->DefPageSize = $size;
 	$this->CurPageSize = $size;
