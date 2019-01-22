34f317af7fe6b9d21d4abdc409c66b2c16909e10
Vittorio Emmermann <emmermann@mycary.de>
2019-01-22 17:46:35 +0100

    Changed define to optional
    If you have FPDF over annother package defined, you should not define it here. contained it with an if.

--- src/Fpdf/Fpdf.php
+++ src/Fpdf/Fpdf.php
@@ -7,7 +7,10 @@
 * Author:  Olivier PLATHEY                                                     *
 *******************************************************************************/
 
-define('FPDF_VERSION','1.81');
+
+if (!defined('FPDF_VERSION')) {
+	define('FPDF_VERSION','1.81');
+}
 
 class Fpdf
 {
