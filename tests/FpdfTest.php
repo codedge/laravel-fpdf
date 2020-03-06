7bea9f0129158c4ecd8f0c269fc46d46e1399fae
Holger Lösken <hl@inquence.com>
2020-03-06 15:12:12 +0100

    Add tests

---
+++ tests/FpdfTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Codedge\Fpdf\Test;
+
+use Codedge\Fpdf\Fpdf\Fpdf;
+
+class FpdfTest extends TestCase
+{
+    protected $fpdf;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->fpdf = new Fpdf();
+    }
+
+    /** @test */
+    public function it_can_instantiate()
+    {
+        $this->assertInstanceOf(Fpdf::class, $this->fpdf);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace Codedge\Fpdf\Test;
+
+use Codedge\Fpdf\FpdfServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+abstract class TestCase extends Orchestra
+{
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array|string[]
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            FpdfServiceProvider::class,
+        ];
+    }
+}
